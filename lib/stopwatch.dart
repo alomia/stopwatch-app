cb858a840a657c316032d8d97c5ba446a6e25156
Alejandro Alomia <alejandroalomia730@gmail.com>
2024-03-10 00:06:02 -0500

    interacting with buttons

--- lib/stopwatch.dart
+++ lib/stopwatch.dart
@@ -12,12 +12,7 @@ class StopWatch extends StatefulWidget {
 class _StopWatchState extends State<StopWatch> {
   int seconds = 0;
   late Timer timer;
-
-  @override
-  void initState() {
-    super.initState();
-    timer = Timer.periodic(const Duration(seconds: 1), _onTick);
-  }
+  bool _isTicking = false;
 
   void _onTick(Timer time) {
     if (mounted) {
@@ -32,18 +27,65 @@ class _StopWatchState extends State<StopWatch> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text(
-          "StopWatch",
-          style: TextStyle(fontWeight: FontWeight.w500),
-        ),
+        title: const Text("StopWatch"),
       ),
-      body: Center(
-        child: Text(
-          "$seconds ${_secondsText()}",
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "$seconds ${_secondsText()}",
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          const SizedBox(height: 20.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)))),
+                onPressed: _isTicking ? null : _startTimer,
+                child: const Text("Start"),
+              ),
+              const SizedBox(width: 20.0),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.red),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)))),
+                onPressed: _isTicking ? _stopTimer : null,
+                child: const Text("Stop"),
+              )
+            ],
+          )
+        ],
       ),
     );
+  }
+
+  void _startTimer() {
+    timer = Timer.periodic(const Duration(seconds: 1), _onTick);
+
+    setState(() {
+      seconds = 0;
+      _isTicking = true;
+    });
+  }
+
+  void _stopTimer() {
+    timer.cancel();
+    setState(() {
+      _isTicking = false;
+    });
   }
 
   String _secondsText() => seconds == 1 ? "Second" : "Seconds";
